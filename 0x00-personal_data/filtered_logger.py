1b4d94abe7390790d41565b53f596c7c668d52a1
Gadoskey <mustopha.yusufope@gmail.com>
2024-11-07 14:56:03 +0000

    ok

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -11,7 +11,6 @@ from typing import List
 
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
-    """ filter_datum that returns a log message obfuscated """
     p = r'(' + '|'.join([f'{field}=[^;]*' for field in fields]) + ')'
     return re.sub(
             p, lambda m: m.group(0).split('=')[0] + '=' + redaction, message)
