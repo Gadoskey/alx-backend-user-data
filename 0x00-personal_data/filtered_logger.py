4f2209e3cc36722d8fc9ea6414d00c2d32e0cc1f
Gadoskey <gadoski321@gmail.com>
2024-11-07 17:16:00 +0100

    still debugging

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -9,6 +9,9 @@ import re
 from typing import List
 
 
-def filter_datum(fields: List[str], redaction: str, message: str,
-                 separator: str) -> str:
-    return re.sub(r'(' + '|'.join([f'{field}=[^;]*' for field in fields]) + ')', lambda m: f"{m.group(0).split('=')[0]}={redaction}", message)
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    for f in fields:
+        r = re.sub(f'{f}=.*?{separator}',
+                         f'{f}={redaction}{separator}', message)
+    return r
