6e5eee0b1861b384b1bf39529e9025e190f23135
Gadoskey <mustopha.yusufope@gmail.com>
2024-11-07 14:58:31 +0000

    ok

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -12,5 +12,4 @@ from typing import List
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
     p = r'(' + '|'.join([f'{field}=[^;]*' for field in fields]) + ')'
-    return re.sub(
-            p, lambda m: m.group(0).split('=')[0] + '=' + redaction, message)
+    return re.sub(p, lambda m: m.group(0).split('=')[0] + '=' + redaction, message)
