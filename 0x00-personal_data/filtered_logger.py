e7573fd8f00b69333ff367526b799d770f71b864
Gadoskey <mustopha.yusufope@gmail.com>
2024-11-07 14:26:58 +0000

    ok

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 """
 Author: Gadoskey
-Description: This module contains a function `filter_datum` that returns a log message obfuscated.
+Description: This module contains a function `filter_datum`
+that returns a log message obfuscated.
 """
 import logging
 import re
 from typing import List
 
 
-def filter_datum(self, fields: List[str], redaction: str, message: str, separator: str):
+def filter_datum(fields: List[str], redaction: str,message: str, separator: str) -> str:
     """ filter_datum that returns a log message obfuscated """
     return re.sub(r'(' + '|'.join([f'{field}=[^;]*' for field in fields]) + ')',
                   lambda m: m.group(0).split('=')[0] + '=' + redaction, message)
