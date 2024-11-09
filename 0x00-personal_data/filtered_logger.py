c4940ffade371e52206a751129e7e77347d4a4d7
Gadoskey <gadoski321@gmail.com>
2024-11-09 11:14:30 +0100

    task 1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -13,3 +13,21 @@ def filter_datum(fields: List[str], redaction: str, message: str, separator: str
     """Obfuscate specified fields in the log message."""
     p = r'|'.join([f'{field}=.*?{separator}' for field in fields])
     return re.sub(p, lambda m: f"{m.group(0).split('=')[0]}={redaction}{separator}", message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        record.msg = filter_datum(
+            self.fields, self.REDACTION, record.getMessage(), self.SEPARATOR)
+        return super().format(record)
