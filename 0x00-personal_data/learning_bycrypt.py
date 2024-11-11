5fdc7e5a8b36ebd66425c0eb5d3d044ede4a9e88
Gadoskey <gadoski321@gmail.com>
2024-11-11 14:49:36 +0100

    learning bycrypt

---
+++ 0x00-personal_data/learning_bycrypt.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Author: Gadoskey
+Description: Just playing around with bycrypt looooolll
+"""
+
+
+import bcrypt
+
+# Password to hash
+password = "reecejames"
+
+# Generate the salt and hash the password
+salt = bcrypt.gensalt()
+hashed_password = bcrypt.hashpw(password.encode(), salt)
+
+# Save the hashed password to a file
+with open("hashed_password.txt", "wb") as f:
+    f.write(hashed_password)
+
+print("Password has been hashed and stored in 'hashed_password.txt'.")
