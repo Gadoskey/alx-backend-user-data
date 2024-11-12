718a83ad0e132f195e5d7b210f745beb536d0fde
Gadoskey <gadoski321@gmail.com>
2024-11-12 14:50:02 +0100

    removed blank lines

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -27,8 +27,6 @@ def forbidden_error(error) -> str:
     return jsonify({"error": "Forbidden"}), 403
 
 
-
-
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
