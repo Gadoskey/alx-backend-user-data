323d07afd1d18001ae55e898c71b6a601f2054b5
Gadoskey <gadoski321@gmail.com>
2024-11-12 14:58:06 +0100

    task 3

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,17 @@
+from flask import request
+
+class Auth:
+  
+  def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+    """ hvdhvdh"""
+    return False
+  
+  
+  def authorization_header(self, request=None) -> str:
+    """dhdhdh"""
+    return None
+  
+  
+  def current_user(self, request=None) -> TypeVar('User'):
+    """hdhdh"""
+    return None
