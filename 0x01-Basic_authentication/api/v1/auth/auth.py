fd3b0ba2e0811b83ea9b88a3fa629eda35704938
Gadoskey <mustopha.yusufope@gmail.com>
2024-11-12 14:10:48 +0000

    ok

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -3,16 +3,16 @@
 from flask import request
 from typing import List, TypeVar
 
+
 class Auth:
+    """ Class Auth """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Method that determines if authentication is required """
         return False
 
-
     def authorization_header(self, request=None) -> str:
         """ Method that returns authorization header """
         return None
-
 
     def current_user(self, request=None) -> TypeVar('User'):
         """ Method that returns the current user """
