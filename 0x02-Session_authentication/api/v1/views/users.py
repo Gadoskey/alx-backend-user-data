ad6c2068a56aec7347145b9b08a3b8850ad899ba
Gadoskey <mustopha.yusufope@gmail.com>
2024-11-16 16:03:02 +0000

    ok

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -29,14 +29,14 @@ def view_one_user(user_id: str = None) -> str:
         abort(404)
     if user_id == 'me' and request.current_user is None:
         abort(404)
-    
+
     user = User.get(user_id)
     if user is None:
         abort(404)
-        
+
     if user_id == 'me' and request.current_user is not None:
-        return (user.to_json())
-        
+        return jsonify(user.to_json())
+
     return jsonify(user.to_json())
 
 
