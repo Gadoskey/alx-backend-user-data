ce0b6de876b9596bad999a0332d49410c1b55c1d
Gadoskey <gadoski321@gmail.com>
2024-11-18 13:25:47 +0100

    kkkk

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -49,8 +49,8 @@ def before_request():
     if (
         auth.authorization_header(request) is None and
         auth.session_cookie(request) is None
-        ):
-            abort(401)  # Unauthorized
+    ):
+        abort(401)  # Unauthorized
 
     # Check if a valid user is returned
     current_user = auth.current_user(request)
