4d3d06f9c2565954e71fda8b269c6a3d4f7f9586
Gadoskey <gadoski321@gmail.com>
2024-11-20 09:32:49 +0100

    uuuu

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -11,6 +11,9 @@ Base = declarative_base()
 
 
 class User(Base):
+    """
+      Class User that inherits from Base
+    """
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True, autoincrement=True)
     email = Column(String(250), nullable=False)
