edd4374dceaa08f1a440bbceb7a2bc6af7540a56
Gadoskey <gadoski321@gmail.com>
2024-11-20 09:27:21 +0100

    kkkk

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """
   Author: Yusuf Mustapha Opeyemi
-  Desc: A SQLAlchemy model named User for a database table named users.
+  Desc: User model for a database table named 'users'.
 """
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
@@ -12,7 +12,7 @@ Base = declarative_base()
 
 class User(Base):
     __tablename__ = 'users'
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
