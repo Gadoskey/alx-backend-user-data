765170e604b9152fcb44e13ec8b8f95e82e64dd9
Gadoskey <gadoski321@gmail.com>
2024-11-20 10:02:04 +0100

    task 1

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 """ DB module
     Author: Yusuf Mustapha Opeyemi
 """
@@ -6,7 +5,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-
 from user import Base, User
 
 
