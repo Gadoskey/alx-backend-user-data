8581f1c23f1739f451b73c1fc375375964023560
Gadoskey <gadoski321@gmail.com>
2024-11-21 23:30:21 +0100

    999999---

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -25,7 +25,7 @@ def _hash_password(password: str) -> bytes:
     return hashed_password
 
 
-def _generate_uuid(self) -> str:
+def _generate_uuid() -> str:
     """
     Generates a new unique UUID.
 
