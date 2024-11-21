47c4d43f8b3da8fd1758ce014262b41f974c2f90
Gadoskey <gadoski321@gmail.com>
2024-11-21 22:01:50 +0100

    task 6

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -7,9 +7,10 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 
-@app.route('/', methods = ['GET'])
+@app.route('/', methods=['GET'])
 def homepage():
     return jsonify({"message": "Bienvenue"})
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
