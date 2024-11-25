d76f1a0de974456cf443c54482925c1bd9aaadbb
Gadoskey <gadoski321@gmail.com>
2024-11-25 08:10:40 +0100

    17

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -97,5 +97,25 @@ def profile():
         abort(403)
 
 
+@app.route('/reset_password', methods=['POST'])
+def get_reset_password_token():
+    """
+    POST /reset_password
+    Reset a user's password.
+
+    Returns:
+        JSON response with appropriate message and status code.
+    """
+    email = request.form.get("email")
+    if not email:
+        abort(403)
+    try:
+        user = AUTH._db.find_user_by(email=email)
+        reset_token = AUTH.get_reset_password_token
+        return jsonify({"email": email, "reset_token": reset_token}), 200
+    except NoResultFound:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
